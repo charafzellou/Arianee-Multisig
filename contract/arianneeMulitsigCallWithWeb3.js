2e2147941a39223ba7708c3483efdf0ff8875378
jeigonin <j.igonin.63@gmail.com>
2019-11-20 17:10:08 +0100

    Modify typo error

--- contract/arianneeMulitsigCallWithWeb3.js
+++ contract/arianneeMulitsigCallWithWeb3.js
@@ -13,33 +13,30 @@ let arianeeContract = web3.Eth.GetContract(MultiSigExample.Properties.Resources.
 
 //First step : Approve multisig in Arianee contract
 //create a call instance for approve function of the arianee contract
-let arianeeTransferFunction = arianeeContract.GetFunction("approve");
+let arianeeApproveFunction = arianeeContract.GetFunction("approve");
 //multisig contract addrass as data for approve function 
-let data = arianeeTransferFunction.CreateCallInput(multiSigContractAddress).Data;
-let byteDataApprove = data.HexToByteArray();
+let dataApprove = arianeeApproveFunction.CreateCallInput(multiSigContractAddress).Data;
+let byteDataApprove = dataApprove.HexToByteArray();
 //call transaction 
 let gasApprove = await multiSigExecuteFunction.EstimateGasAsync(arianeeContractAddress, 0, byteDataApprove);
 //result of transaction is in transactionHash below
-let transactionHash = await multiSigExecuteFunction.SendTransactionAsync(account.Address, gasApprove, null, arianeeContractAddress, 0, byteDataApprove);
+let transactionHashArianeeApprove = await multiSigExecuteFunction.SendTransactionAsync(account.Address, gasApprove, null, arianeeContractAddress, 0, byteDataApprove);
 
 //Second step : start Transfer function in Arianee contract
 //new address for arianee certification
 let newAddress = "0xf8318fC2BDDc6F46F356EcBEe300064213DA9FA9";
-//arianee contract address
-let arianeeContractAddress = "0xf8318fC2BDDc6F46F356EcBEe300064213DA9FA5";
-let arianeeContract = web3.Eth.GetContract(MultiSigExample.Properties.Resources.arianeeContractABI, arianeeContractAddress);
 //create a call instance for Transfer function of the arianee contract
 let arianeeTransferFunction = arianeeContract.GetFunction("Transfer");
 //data for Transfer function
-let data = arianeeTransferFunction.CreateCallInput(newAddress).Data;
-let byteDataTransfer = data.HexToByteArray();
+let dataTransfer = arianeeTransferFunction.CreateCallInput(newAddress).Data;
+let byteDataTransfer = dataTransfer.HexToByteArray();
 //call transaction 
 let gasTransfer = await multiSigExecuteFunction.EstimateGasAsync(arianeeContractAddress, 0, byteDataTransfer);
-let transactionHash = await multiSigExecuteFunction.SendTransactionAsync(account.Address, gasTransfer, null, arianeeContractAddress, 0, byteDataTransfer);
+let transactionHashArianneeTransfer = await multiSigExecuteFunction.SendTransactionAsync(account.Address, gasTransfer, null, arianeeContractAddress, 0, byteDataTransfer);
 
 
 //Third end last step : confirm in multisig the transaction of the Transfer call 
 //Multisig validation //need to be done with all address that should confirm the transaction
 let multiSigExecuteFunction = multiSigContract.GetFunction("executeTransaction");
 let gas = await multiSigExecuteFunction.EstimateGasAsync(arianeeContractAddress, 0, byteDataTransfer);
-let transactionHash = await multiSigExecuteFunction.SendTransactionAsync(account.Address, gas, null, arianeeContractAddress, 0, byteDataTransfer);
+let transactionHashMultisig = await multiSigExecuteFunction.SendTransactionAsync(account.Address, gas, null, arianeeContractAddress, 0, byteDataTransfer);
