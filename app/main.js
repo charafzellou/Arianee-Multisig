838af1ef880fd5c8000921d0d3d96cc91be556ed
Vincent <louis-vanhassel@outlooK.fr>
2019-11-21 00:18:37 +0100

    front finish

--- app/main.js
+++ app/main.js
@@ -1,36 +1,88 @@
 const list = document.getElementById("list");
 let value = 3;
 
+let signature = [];
+let multisig = [];
+let certif = [];
+let owner = [];
+
 function add() {
-    value += 1;
-    list.innerHTML += `
-        <div class="form-group" id="key-`+ value +`">
-            <input class="form-control form-control-lg" type="text" placeholder="key n°` + value +`">
+    if(value < 200) {
+        value += 1;
+        list.innerHTML += `
+        <div class="form-group" id="group-form-` + value + `">
+            <input class="form-control form-control-lg" id="key-` + value + `" type="text" placeholder="pubKey n°` + value + `">
         </div>`;
-    value += 1;
-    list.innerHTML += `
-        <div class="form-group" id="key-`+ value + `">
-            <input class="form-control form-control-lg" type="text" placeholder="key n°` + value + `">
+        value += 1;
+        list.innerHTML += `
+        <div class="form-group" id="group-form-` + value + `">
+            <input class="form-control form-control-lg" id="key-` + value + `" type="text" placeholder="pubKey n°` + value + `">
         </div>`;
+    }
     return value;
 }
 
 function remove() {
-    console.log(document.getElementById(`key-` + value))
-    document.getElementById(`key-` + value)  ;
-    value -= 1;
-    document.getElementById(`key-` + value).remove;
-    value -= 1;
+    if(value > 3) {
+        document.getElementById('group-form-' + value).outerHTML = "";
+        value -= 1;
+        document.getElementById('group-form-' + value).outerHTML = "";
+        value -= 1;
+    }
     return value;
 }
 
-Element.prototype.remove = function () {
-    this.parentElement.removeChild(this);
-}
-NodeList.prototype.remove = HTMLCollection.prototype.remove = function () {
-    for (var i = this.length - 1; i >= 0; i--) {
-        if (this[i] && this[i].parentElement) {
-            this[i].parentElement.removeChild(this[i]);
+function signatureForm() {
+    signature = []
+    for(let x = 1; x <= value; x++) {
+        if(document.getElementById('key-' + value) == null) {
+            signature = [];
+            document.getElementById('warning-signature').innerHTML = '<h6>toutes les signatures ne sont pas présentent</h6>';
+            return;
+        }
+        else {
+            signature.push(document.getElementById(`key-` + x).value);
         }
     }
+    console.log(signature);
+    return signature;
+}
+
+function multisigApprove() {
+    multisig = [];
+    if (document.getElementById('privateKey').value) {
+        if (document.getElementById('multiSig').value) {
+            multisig.push(document.getElementById('privateKey').value);
+            multisig.push(document.getElementById('multiSig').value);
+        }
+    } else {
+        document.getElementById('warning-multisig').innerHTML = '<h6>les input sont vides</h6>';
+    }
+    console.log(multisig);
+    return multisig;
+}
+
+function multisigMigrate() {
+    certif = [];
+    if (document.getElementById('certif').value) {
+        if (document.getElementById('newAddresse').value) {
+            certif.push(document.getElementById('certif').value);
+            certif.push(document.getElementById('newAddresse').value);
+        }
+    } else {
+        document.getElementById('warning-certif').innerHTML = '<h6>les input sont vides</h6>';
+    }
+    console.log(certif);
+    return certif;
+}
+
+function newOwner() {
+    owner = [];
+    if (document.getElementById('ownerKey').value) {
+        owner.push(document.getElementById('ownerKey').value)
+    } else {
+        document.getElementById('warning-owner').innerHTML = '<h6>aucune clef privée</h6>';
+    }
+    console.log(owner)
+    return owner;
 }
